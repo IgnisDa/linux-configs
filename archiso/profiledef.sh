ca408d7f842a9b6be2001600a8093f94b948a9bc
IgnisDa <ignisds2002@gmail.com>
2020-09-03 12:05:22 +0530

    Add fish config

--- archiso/profiledef.sh
+++ archiso/profiledef.sh
@@ -2,7 +2,7 @@
 # shellcheck disable=SC2034
 
 iso_name="archlinux"
-iso_label="ARCH_$(date +%Y%m)"
+iso_label="ARCH"
 iso_publisher="Arch Linux <https://www.archlinux.org>"
 iso_application="Arch Linux Live/Rescue CD"
 iso_version="$(date +%Y.%m.%d)"
